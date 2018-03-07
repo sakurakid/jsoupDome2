58075d3c3263555c774f502f751edd695be39488
sakurakid <610444099@qq.com>
2018-03-07 22:30:06 +0800

    练习2

--- app/src/main/java/com/example/hasee/jsoupdome2/MainActivity.java
+++ app/src/main/java/com/example/hasee/jsoupdome2/MainActivity.java
@@ -9,6 +9,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private TextView show;
     private String s;
@@ -19,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         show = (TextView) findViewById(R.id.text);
-        get();
-
+        //get();
+        get2();
 
 }
+//在这里Documnet继承于   Element 继承于 Node类
+// Jsoup.connect(String url)方法不能运行在主线程，否则会报NetworkOnMainThreadException
    private void get(){
                new Thread(new Runnable() {
                    @Override
@@ -38,9 +42,43 @@ public class MainActivity extends AppCompatActivity {
                    }
                }).start();
            }
+    private void get2(){
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               try {
+                   Document doc = Jsoup.connect("http://home.meishichina.com/show-top-type-recipe.html").get();
+                   Elements titleAnd = doc.select("div.pic");
+                   Log.i("mytag","title:"+titleAnd.get(1).select("a").attr("title")+"pic:"+titleAnd.get(1).select("a").select("img").attr("data-src"));
 
-   
+                   Elements url = doc.select("a");
+                   Log.i("mytag","url"+url.get(1).attr("href"));
 
+                   Elements burden = doc.select("p.subcontent");
+                   Log.i("mytag","burden"+burden.get(1).text());
+
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+
+           }
+       }).start();
+    }
+
+
+/*
+1.下载jar包并丢到libs(或者在gradle)
+2.找到心仪的网页
+3.用Jsoup.connect()获取网页的document
+4.查看网页源码，对准你想要的地方，给他来一个Element.select(String selector)
+5.用Node.attr(String key)或者Element.text()方法把数据抽出来
+
+
+作者：四会歌神陈子豪
+链接：https://www.jianshu.com/p/a620a2664f58
+來源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+* */
 
 
 
